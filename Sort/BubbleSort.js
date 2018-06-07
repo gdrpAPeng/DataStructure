6a512bd1a88a820c7025aaae422377df146ed535
APeng <522731323@qq.com>
2018-06-07 22:32:16 +0800

    更新冒泡排序、选择排序

---
+++ Sort/BubbleSort.js
@@ -0,0 +1,27 @@
+/**
+ * 冒泡排序
+ * 2018-06-07 21:36
+ */
+
+/**
+ * 算法描述
+ *   比较相邻的元素。如果第一个比第二个大，就交换它们两个；
+ *   对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对，这样在最后的元素应该会是最大的数；
+ *   针对所有的元素重复以上的步骤，除了最后一个；
+ *   重复步骤1~3，直到排序完成。
+ */
+function bubbleSort(arr) {
+    let len = arr.length;
+    for(let i = 0; i < len; i++) {
+        for(let j = 0; j < len-1-i; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+let arr = [1,3,5,2,8,6];
+console.log(bubbleSort(arr));
---
+++ Sort/SelectionSort.js
@@ -0,0 +1,29 @@
+/**
+ * 选择排序
+ */
+/**
+ *  算法描述
+ *      首先在未排序序列中找到最小（大）元素
+ *      存放到排序序列的起始位置
+ *      再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾
+ */
+ function selectionSort(arr) {
+     let len = arr.length;
+     for(let i = 0; i < len-1; i++ ) {
+         let min = arr[i];      // 将数组第一个元素初始化为最小值
+         let minIndex = i;      // 标记第一个元素的下标
+         for(let j = i+1; j < len; j++) { 
+            if(arr[j] < min) {      // 寻找最小元素
+                min = arr[j];
+                minIndex = j;
+            }
+         }
+         if(minIndex!=i){
+             arr[minIndex] = arr[i];
+             arr[i] = min;
+         }
+     }
+     return arr;
+ }
+ let arr = [9,3,5,2,8,6];
+ console.log(selectionSort(arr));
